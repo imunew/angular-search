207bef8e163d9227415f3c9e2dcef21400c932b7
imura <imura@hitomedia.jp>
2014-03-15 22:10:09 +0900

    First Commit

---
+++ youtube/controllers.js
@@ -0,0 +1,17 @@
+
+
+function YoutubeSearch($scope, $http) {
+  $scope.doSearch = function () {
+    
+    var uri ='http://gdata.youtube.com/feeds/api/videos';
+    var query = {
+      "lr": "ja", "alt": "json", "callback": "JSON_CALLBACK",
+      "vq": $scope.query
+    };
+    uri = uri + "?" + $.param(query);
+    
+    $http.jsonp(uri).success(function(data) {
+      $scope.results = data;
+    });
+  };
+}
